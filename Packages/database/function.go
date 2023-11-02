4b06a39ee683ad362677fe92b83b25b58a39a0ac
Kae-Tempest <katsuhiko.tempest@gmail.com>
2023-11-02 22:15:53 +0100

    Finish fix Dto func

--- Packages/database/function.go
+++ Packages/database/function.go
@@ -4,6 +4,7 @@ import (
 	"RaphaelGo/Packages/HandleError"
 	"RaphaelGo/Packages/Struct"
 	"context"
+	"database/sql"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
 )
@@ -19,24 +20,24 @@ func CreatePlayer(s *discordgo.Session, player Struct.Player) {
 		HandleError.SendDBError(s, errMessage)
 		return
 	} else {
-
+		var id int
 		err := db.QueryRow(context.Background(),
-			`insert into public.players (username,level,exp,po,job_id,rank,city_id) values ($1,$2,$3,$4,$5,$6,$7)`,
-			player.Username, player.Level, player.EXP, player.PO, player.Job.ID, player.Rank, player.City.ID)
+			`insert into public.players (username,level,exp,po,job_id,rank,city_id) values ($1,$2,$3,$4,$5,$6,$7) RETURNING id;`,
+			player.Username, player.Level, player.EXP, player.PO, player.Job.ID, player.Rank, player.City.ID).Scan(&id)
 		if err != nil {
 			errMessage := fmt.Errorf("cannot create Player: %s", err).Error()
 			HandleError.SendDBError(s, errMessage)
 		}
-		err = db.QueryRow(context.Background(),
-			`insert into public.attributes (player_id, hp,mp,strength,agility,intelligence,spirit,constitution,speed,luck) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)`,
-			player.ID, player.Attribute.HP, player.Attribute.MP, player.Attribute.Strength, player.Attribute.Agility, player.Attribute.Intelligence, player.Attribute.Spirit, player.Attribute.Constitution, player.Attribute.Speed, player.Attribute.Luck)
+		_, err = db.Exec(context.Background(),
+			`insert into public.attributes (player_id, hp,mp,strength,agility,intelligence,spirit,constitution,speed,luck) values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10);`,
+			id, player.Attribute.HP, player.Attribute.MP, player.Attribute.Strength, player.Attribute.Agility, player.Attribute.Intelligence, player.Attribute.Spirit, player.Attribute.Constitution, player.Attribute.Speed, player.Attribute.Luck)
 		if err != nil {
 			errMessage := fmt.Errorf("cannot create Attributes: %s", err).Error()
 			HandleError.SendDBError(s, errMessage)
 		}
-		err = db.QueryRow(context.Background(),
-			`insert into public.drop_rank (player_id,plants,animals,minerals,magic,special) values ($1,$2,$3,$4,$5,$6)`,
-			player.ID, player.DropRank.Plants, player.DropRank.Animals, player.DropRank.Minerals, player.DropRank.Magic, player.DropRank.Special)
+		_, err = db.Exec(context.Background(),
+			`insert into public.drop_rank (player_id,plants,animals,minerals,magic,special) values ($1,$2,$3,$4,$5,$6);`,
+			id, player.DropRank.Plants, player.DropRank.Animals, player.DropRank.Minerals, player.DropRank.Magic, player.DropRank.Special)
 		if err != nil {
 			errMessage := fmt.Errorf("cannot create DropRank: %s", err).Error()
 			HandleError.SendDBError(s, errMessage)
@@ -110,7 +111,7 @@ func GetJobByName(s *discordgo.Session, name string) Struct.Job {
 func getCityById(s *discordgo.Session, id int) Struct.City {
 	var city Struct.City
 	err := db.QueryRow(context.Background(),
-		`select * from public.cities where id = $1`, id).Scan(&city.ID, &city.Name, &city.Abandoned, &city.Rank, &city.Description)
+		`select * from public.cities where id = $1`, id).Scan(&city.ID, &city.Name, &city.Rank, &city.Abandoned, &city.Description)
 	if err != nil {
 		if err.Error() == "no rows in result set" {
 			return city
@@ -134,8 +135,10 @@ func GetCityByName(s *discordgo.Session, name string) Struct.City {
 
 func getPlayerAttributes(s *discordgo.Session, id int) Struct.Attribute {
 	var attribute Struct.Attribute
+	var playerId int
+	var itemId sql.NullInt16
 	err := db.QueryRow(context.Background(),
-		`select * from public.attributes where player_id = $1`, id).Scan(&attribute.HP, &attribute.MP, &attribute.Strength, &attribute.Agility, &attribute.Intelligence, &attribute.Spirit, &attribute.Constitution, &attribute.Speed, &attribute.Luck)
+		`select * from public.attributes where player_id = $1`, id).Scan(&playerId, &itemId, &attribute.HP, &attribute.MP, &attribute.Strength, &attribute.Agility, &attribute.Intelligence, &attribute.Spirit, &attribute.Constitution, &attribute.Speed, &attribute.Luck)
 	if err != nil {
 		if err.Error() == "no rows in result set" {
 			return attribute
@@ -148,8 +151,9 @@ func getPlayerAttributes(s *discordgo.Session, id int) Struct.Attribute {
 
 func getPlayerDropRank(s *discordgo.Session, id int) Struct.DropRank {
 	var dropRank Struct.DropRank
+	var player_id int
 	err := db.QueryRow(context.Background(),
-		`select * from public.drop_rank where player_id = $1`, id).Scan(&dropRank.Plants, &dropRank.Animals, &dropRank.Minerals, &dropRank.Magic, &dropRank.Special)
+		`select * from public.drop_rank where player_id = $1`, id).Scan(&player_id, &dropRank.Plants, &dropRank.Animals, &dropRank.Minerals, &dropRank.Magic, &dropRank.Special)
 	if err != nil {
 		if err.Error() == "no rows in result set" {
 			return dropRank
--- Packages/rpg/main.go
+++ Packages/rpg/main.go
@@ -4,13 +4,9 @@ import (
 	"RaphaelGo/Packages/HandleError"
 	"RaphaelGo/Packages/Struct"
 	"RaphaelGo/Packages/database"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
-	"io"
 	"math/rand"
-	"os"
 	"strconv"
 	"time"
 )
@@ -57,97 +53,8 @@ func Setup(s *discordgo.Session, m *discordgo.MessageCreate, start time.Time, ar
 	}
 	// insert Player to db
 	database.CreatePlayer(s, p)
-	player := database.GetPlayerById(s, p.ID)
+	player := database.GetPlayerByName(s, p.Username)
 	fmt.Println("Player", player)
 	//fmt.Println("Player", p)
 	fmt.Println("pinged in", timeElapsed)
-}
-
-func getCityInfo(ResearchCity string, m *discordgo.MessageCreate, s *discordgo.Session) (Struct.City, error) {
-	var selectedCity Struct.City
-	CityFind := false
-	jsonFile, err := os.Open("./assets/city.json")
-	if err != nil {
-		return selectedCity, fmt.Errorf("error: %s", err)
-	}
-	defer func(jsonFile *os.File) {
-		err := jsonFile.Close()
-		if err != nil {
-			errMessage := fmt.Errorf("error during closing json file: %s", err).Error()
-			HandleError.SendLogError(s, m, errMessage)
-		}
-	}(jsonFile)
-	byteValue, _ := io.ReadAll(jsonFile)
-	var Citys []Struct.City
-	err = json.Unmarshal(byteValue, &Citys)
-	if err != nil {
-		HandleError.SendLogError(s, m, fmt.Errorf("parsing error for city: %s", err).Error())
-		return Struct.City{}, err
-	}
-	for _, city := range Citys {
-		if city.Name == ResearchCity && !city.Abandoned {
-			selectedCity = city
-			CityFind = true
-			break
-		} else if city.Abandoned {
-			_, err := s.ChannelMessageSend(m.ChannelID, "City is abandoned")
-			if err != nil {
-				errMessage := fmt.Errorf("an Error as occurend when sending message").Error()
-				HandleError.SendLogError(s, m, errMessage)
-				return Struct.City{}, err
-			}
-			return selectedCity, errors.New("abandoned")
-		}
-	}
-	if !CityFind {
-		_, err := s.ChannelMessageSend(m.ChannelID, "Any city Found")
-		if err != nil {
-			errMessage := fmt.Errorf("an Error as occurend when sending message").Error()
-			HandleError.SendLogError(s, m, errMessage)
-			return Struct.City{}, err
-		}
-		return selectedCity, errors.New("any city found")
-	}
-	return selectedCity, nil
-}
-
-func getJobInfo(ResearchJob string, m *discordgo.MessageCreate, s *discordgo.Session) (Struct.Job, error) {
-	var SelectedJob Struct.Job
-	JobFind := false
-	jsonFile, err := os.Open("./assets/job.json")
-	if err != nil {
-		return SelectedJob, fmt.Errorf("error: %s", err)
-	}
-	defer func(jsonFile *os.File) {
-		err := jsonFile.Close()
-		if err != nil {
-			errMessage := fmt.Errorf("error during closing json file: %s", err).Error()
-			HandleError.SendLogError(s, m, errMessage)
-		}
-	}(jsonFile)
-	byteValue, _ := io.ReadAll(jsonFile)
-	var jobs []Struct.Job
-	err = json.Unmarshal(byteValue, &jobs)
-	if err != nil {
-		HandleError.SendLogError(s, m, fmt.Errorf("parsing error for job: %s", err).Error())
-		return Struct.Job{}, err
-	}
-	for _, job := range jobs {
-		if job.Name == ResearchJob {
-			SelectedJob = job
-			JobFind = true
-			break
-		}
-	}
-	if !JobFind {
-		_, err := s.ChannelMessageSend(m.ChannelID, "Any Job Find")
-		if err != nil {
-			errMessage := fmt.Errorf("an Error as occurend when sending message").Error()
-			HandleError.SendLogError(s, m, errMessage)
-			return Struct.Job{}, err
-		}
-		return SelectedJob, errors.New("any job find")
-	}
-
-	return SelectedJob, nil
 }
